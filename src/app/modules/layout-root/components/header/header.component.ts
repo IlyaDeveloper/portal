c71110a339c92ecb375c5f7fa94957a128224ed6
Илья Пащук <illyadeveloper@gmail.com>
2022-01-19 10:07:35 +0200

    Stage: added header

--- src/app/modules/layout-root/components/header/header.component.ts
+++ src/app/modules/layout-root/components/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+
+const STATUS_PAYMENT = [
+  'Status',
+  'Active',
+  'Preload',
+  'Wait',
+  'Done'
+]
 
 @Component({
   selector: 'prt-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  filteredType: string = 'down';
+  statusList: any = [];
 
-  ngOnInit(): void {
+  constructor() {
+    this.statusList = STATUS_PAYMENT;
   }
 
+  sorted(): void {
+    (this.filteredType === 'down')
+      ? this.filteredType = 'up'
+      : this.filteredType = 'down';
+  }
 }
--- src/app/ui/components/tag/tag.component.ts
+++ src/app/ui/components/tag/tag.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'prt-tag',
   templateUrl: './tag.component.html',
   styleUrls: ['./tag.component.scss']
 })
-export class TagComponent implements OnInit {
+export class TagComponent {
 
-  constructor() { }
+  isSelected: boolean = false;
 
-  ngOnInit(): void {
+  constructor() {
+  }
+
+   selectTab():void{
+    this.isSelected = !this.isSelected;
   }
 
 }
